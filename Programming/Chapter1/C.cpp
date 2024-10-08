8e2b42a8f0f419af2c83cdb15f0c99f545032c15
wh <14977299+CarmenDei@user.noreply.gitee.com>
2024-10-08 21:16:05 +0800

    complete C

---
+++ Programming/Chapter1/C.cpp
@@ -0,0 +1,29 @@
+#include "./lib/EquationSolver.hpp"
+#include "./lib/Function.hpp"
+#include<cmath>
+#include<iostream>
+
+using namespace std;
+
+class F:public Function{
+    double operator()(double x) const{
+        return x-tan(x);
+    }
+    double derivative(double x) const{
+        return 1-1/(cos(x)*cos(x));
+    }
+};
+
+void solve_F(double x0){
+    cout << "Solving x-tan(x) near "<< x0 << endl;
+    NewtonMethod F_solver1(F(),x0);
+    double x;
+    x=F_solver1.solve();
+    cout<< "A root is " << x << endl;
+}
+
+int main(){
+    solve_F(4.5);
+    solve_F(7.7);
+    return 0;
+}
